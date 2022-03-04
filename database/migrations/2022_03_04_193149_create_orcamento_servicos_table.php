92365f392bc3bd2f30dc2a3db5fbe420a624cd66
João <joao.pedrolopes10@hotmail.com>
2022-03-04 16:37:24 -0300

    att

---
+++ database/migrations/2022_03_04_193149_create_orcamento_servicos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrcamentoServicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orcamento_servicos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("orcamento_id");
+            $table->unsignedBigInteger("servico_id");
+            $table->integer("qtd");
+            $table->foreign('orcamento_id')->references('id')->on('orcamentos')->onDelete('cascade');
+            $table->foreign('servico_id')->references('id')->on('servicos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orcamento_servicos');
+    }
+}
