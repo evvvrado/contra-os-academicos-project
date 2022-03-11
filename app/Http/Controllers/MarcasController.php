c31ab35116c99f26e60003ac78d56122a8b1e886
João <joao.pedrolopes10@hotmail.com>
2022-03-11 16:51:10 -0300

    att

--- app/Http/Controllers/MarcasController.php
+++ app/Http/Controllers/MarcasController.php
@@ -148,10 +148,10 @@ class MarcasController extends Controller
             $nome_1 = 'imagem.'.$image->getClientOriginalExtension();
             $destinationPath = public_path('/admin/images/marcas/'.$request->id_marca."/");
             $image->move($destinationPath, $nome_1);
-        }
 
-        Marca::where('id', $request->id_marca)
-        ->update(['imagem' => '/admin/images/marcas/'.$request->id_marca."/".$nome_1]);
+            Marca::where('id', $request->id_marca)
+            ->update(['imagem' => '/admin/images/marcas/'.$request->id_marca."/".$nome_1]);
+        }
 
         toastr()->success("Marca salva com sucesso!");
         
