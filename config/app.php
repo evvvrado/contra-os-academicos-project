1d39d8bc7c4a815e6671eca03c292d6aff6965d0
João Pedro <joao.pedrolopes10@hotmail.com>
2022-07-28 15:14:46 -0300

    att

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Dacastro4\LaravelGmail\LaravelGmailServiceProvider::class,
+        // Dacastro4\LaravelGmail\LaravelGmailServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -233,7 +233,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Image' => Intervention\Image\Facades\Image::class,
-        'LaravelGmail' => Dacastro4\LaravelGmail\Facade\LaravelGmail::class,
+        // 'LaravelGmail' => Dacastro4\LaravelGmail\Facade\LaravelGmail::class,
     ],
 
 ];
