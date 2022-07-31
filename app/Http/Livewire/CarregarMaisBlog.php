6d1a03524363eb358e03ae67c01e0d73bfb0b8d9
João <joao.pedrolopes10@hotmail.com>
2022-07-30 22:33:15 -0300

    att

--- app/Http/Livewire/CarregarMaisBlog.php
+++ app/Http/Livewire/CarregarMaisBlog.php
@@ -18,9 +18,6 @@ class CarregarMaisBlog extends Component
     public function render()
     {
         $blogs = DB::select("select * from blogs where status = 1 order by id desc limit ".$this->porpagina);
-        foreach($blogs as $blog){
-            echo $blog->titulo;
-        }
 
         return view('livewire.carregar-mais-blog', ['blogs' => $blogs]);
     }
