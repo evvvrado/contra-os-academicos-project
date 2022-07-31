3e3eb077c836be83020a8e272fb422b6377ddd86
João <joao.pedrolopes10@hotmail.com>
2022-07-30 22:36:08 -0300

    att

--- app/Http/Livewire/CarregarMaisBlog.php
+++ app/Http/Livewire/CarregarMaisBlog.php
@@ -19,6 +19,8 @@ class CarregarMaisBlog extends Component
     {
         $blogs = DB::select("select * from blogs where status = 1 order by id desc limit ".$this->porpagina);
 
+        $blogs= json_decode( json_encode($blogs), true);
+        dd($blogs);
         $blogs = Blog::paginate($this->porpagina)
         ->where('status', 1);
 
